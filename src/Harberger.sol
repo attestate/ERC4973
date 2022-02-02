180d8b855e8a361d62927e43ebfffe8614734a22
Tim Daubenschütz <tim@daubenschuetz.de>
2022-02-02 14:50:10 +0100

    Add block taxation method

--- src/Harberger.sol
+++ src/Harberger.sol
@@ -1,6 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-library Harberger {
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
+struct Percentage {
+  uint256 numerator;
+  uint256 denominator;
+}
+
+struct Period {
+  uint256 start;
+  uint256 end;
+}
+
+library Harberger {
+  function taxByBlock(
+    Percentage percentage,
+    Period period,
+    uint256 price
+  ) pure returns (uint256) {
+    uint256 diff = period.end - period.start;
+    return FixedPointMathLib.fdiv(
+      price * diff * percentage.numerator,
+      percentage.denominator * FixedPointMathLib.WAD,
+      FixedPointMathLib.WAD
+    );
+  }
 }
--- src/Harberger.t.sol
+++ src/Harberger.t.sol
@@ -6,11 +6,8 @@ import {DSTest} from "ds-test/test.sol";
 import {Harberger} from "./Harberger.sol";
 
 contract HarbergerTest is DSTest {
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
-
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function testBlockTax() public {
+      assertEq(tax(0, 50, 1 ether), 0.5 ether);
+      assertEq(tax(0, 1, 1 ether), 0.01 ether);
     }
 }
