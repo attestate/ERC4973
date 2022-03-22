4c7bfe9a4cb576310479c9ac26d5a755ac9c5445
Tim Daubenschütz <tim@daubenschuetz.de>
2022-03-22 11:48:30 +0100

    Add ContinuousAuctioneer contract

--- src/Harberger.sol
+++ src/Harberger.sol
@@ -36,6 +36,72 @@ struct Period {
   uint256 end;
 }
 
+struct Offer {
+  address seller;
+  uint256 startBlock;
+  uint256 startPrice;
+  Perwei taxRate;
+  address token;
+  uint256 tokenId;
+}
+
+contract ContinuousAuctioneer {
+  Offer[] private offers;
+
+  function create(
+    Perwei memory taxRate,
+    address token,
+    uint256 tokenId
+  ) external payable returns (uint256) {
+    require(msg.value > 0, "must send eth");
+    return _create(
+      msg.value,
+      taxRate,
+      token,
+      tokenId
+    );
+  }
+
+  function _create(
+    uint256 startPrice,
+    Perwei memory taxRate,
+    address token,
+    uint256 tokenId
+  ) internal returns (uint256) {
+    Offer memory offer = Offer(
+      msg.sender,
+      block.number,
+      startPrice,
+      taxRate,
+      token,
+      tokenId
+    );
+    offers.push(offer);
+    return offers.length - 1;
+  }
+
+  function bid(
+    uint256 offerId
+  ) external payable returns (uint256) {
+    Offer memory offer = offers[offerId];
+    require(offer.seller != address(0), "offer doesn't exist");
+    uint256 nextPrice = Harberger.pay(
+      offer.taxRate,
+      Period(offer.startBlock, block.number),
+      offer.startPrice
+    );
+
+    payable(offer.seller).transfer(nextPrice);
+    uint256 nextOfferId = _create(
+      msg.value,
+      offer.taxRate,
+      offer.token,
+      offer.tokenId
+    );
+    return nextOfferId;
+  }
+}
+
 library Harberger {
   function pay(
     Perwei memory perwei,
@@ -44,7 +110,7 @@ library Harberger {
   ) internal view returns (uint256 nextPrice) {
     require(msg.value > 0, "must send eth");
     nextPrice = Harberger.getNextPrice(perwei, period, prevPrice);
-    require(msg.value >= nextPrice, "msg.value too low");
+    require(msg.value > nextPrice, "msg.value too low");
   }
 
   function increase(
--- src/Harberger.t.sol
+++ src/Harberger.t.sol
@@ -3,7 +3,12 @@ pragma solidity ^0.8.6;
 
 import {DSTest} from "ds-test/test.sol";
 
-import {Harberger, Period, Perwei} from "./Harberger.sol";
+import {
+  ContinuousAuctioneer,
+  Harberger,
+  Period,
+  Perwei
+} from "./Harberger.sol";
 
 contract PropertyMock {
   function pay(
@@ -26,18 +31,83 @@ contract PropertyMock {
 
 contract HarbergerTest is DSTest {
   PropertyMock p;
+  ContinuousAuctioneer ca;
 
   function setUp() public {
     p = new PropertyMock();
+
+    ca = new ContinuousAuctioneer();
   }
 
-  function testPayInitialPrice() public {
-    Perwei memory perwei1 = Perwei(1, 100);
-    Period memory period1 = Period(0, 0);
-    uint256 price = 1 ether;
+  receive() external payable {}
 
-    uint256 nextPrice = p.pay{value: price}(perwei1, period1, price);
-    assertEq(nextPrice, price);
+  function testBidOnAnOffer() public {
+    uint256 startBlock = block.number;
+    uint256 startPrice = 1 ether;
+    Perwei memory taxRate = Perwei(1, 100);
+    address token = address(1);
+    uint256 tokenId = 0;
+    uint256 offerId0 = ca.create{value: startPrice}(
+      taxRate,
+      token,
+      tokenId
+    );
+
+    uint256 firstBalance = address(this).balance;
+    ca.bid{value: 1.1 ether}(offerId0);
+    uint256 secondBalance = address(this).balance;
+    uint256 endBlock = block.number;
+    assertEq(endBlock-startBlock, 0);
+    assertEq(firstBalance-secondBalance, 0.1 ether);
+    assertEq(address(ca).balance, 1.1 ether);
+  }
+
+  function testFailBidWithFalsePrice() public {
+    uint256 startPrice = 1 ether;
+    Perwei memory taxRate = Perwei(1, 100);
+    address token = address(1);
+    uint256 tokenId = 0;
+    uint256 offerId0 = ca.create{value: startPrice}(
+      taxRate,
+      token,
+      tokenId
+    );
+
+    ca.bid{value: 0.1 ether}(offerId0);
+  }
+
+  function testFailBidOnNonExistentOffer() public {
+    ca.bid(1337);
+  }
+
+  function testFailCreateOfferWithoutValue() public {
+    Perwei memory taxRate = Perwei(1, 100);
+    address token = address(1);
+    uint256 tokenId = 0;
+    ca.create{value: 0}(
+      taxRate,
+      token,
+      tokenId
+    );
+  }
+
+  function testCreateOffer() public {
+    uint256 startPrice = 1 ether;
+    Perwei memory taxRate = Perwei(1, 100);
+    address token = address(1);
+    uint256 tokenId = 0;
+    uint256 offerId0 = ca.create{value: startPrice}(
+      taxRate,
+      token,
+      tokenId
+    );
+    assertEq(offerId0, 0);
+    uint256 offerId1 = ca.create{value: startPrice}(
+      taxRate,
+      token,
+      tokenId
+    );
+    assertEq(offerId1, 1);
   }
 
   function testFailDecreasingAsNonAuthorizedOwner() public {
